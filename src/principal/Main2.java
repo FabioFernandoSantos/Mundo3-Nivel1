f341dadeb3fed217ba39343ef3d8d1abaf370ac4
Julio Marques <juliomarquesjr@yahoo.com.br>
2023-11-18 17:28:51 -0300

    Ajuste no menu e add excluir

--- src/principal/Main2.java
+++ src/principal/Main2.java
@@ -9,6 +9,7 @@ import model.gerenciadores.PessoaFisicaRepo;
 import model.gerenciadores.PessoaJuridicaRepo;
 
 public class Main2 {
+
     private static Scanner scanner = new Scanner(System.in);
     private static PessoaFisicaRepo repoFisica = new PessoaFisicaRepo();
     private static PessoaJuridicaRepo repoJuridica = new PessoaJuridicaRepo();
@@ -25,162 +26,172 @@ public class Main2 {
 
     private static void exibirMenu() {
         System.out.println("Escolha uma opcao:");
-        System.out.println("1 - Incluir");
-        System.out.println("2 - Alterar");
-        System.out.println("3 - Excluir");
-        System.out.println("4 - Exibir pelo ID");
-        System.out.println("5 - Exibir todos");
-        System.out.println("6 - Salvar dados");
-        System.out.println("7 - Recuperar dados");
-        System.out.println("0 - Sair");
-        System.out.println("Digite uma opcao: ");
+        System.out.println("=====================");
+        System.out.println("1 - Incluir Pessoa");
+        System.out.println("2 - Alterar Pessoa");
+        System.out.println("3 - Excluir Pessoa");
+        System.out.println("4 - Buscar pelo ID");
+        System.out.println("5 - Exibir Todos");
+        System.out.println("6 - Persistir Dados");
+        System.out.println("7 - Recuperar Dados");
+        System.out.println("0 - Finalizar Programa");
+        System.out.println("=====================");
+        System.out.print("Digite uma opcao: ");
     }
 
     private static void processarOpcao(int opcao) {
         switch (opcao) {
-            case 1:
-                incluir();
-                break;
-            case 2:
-                alterar();
-                break;
-            case 3:
-                excluir();
-                break;
-            case 4:
-                exibirPorId();
-                break;
-            case 5:
-                exibirTodos();
-                break;
-            case 6:
-                salvarDados();
-                break;
-            case 7:
-                recuperarDados();
-                break;
-            case 0:
-                System.out.println("Finalizando...");
-                break;
-            default:
-                System.out.println("Opção inválida!");
+            case 1 -> incluir();
+            case 2 -> alterar();
+            case 3 -> excluir();
+            case 4 -> exibirPorId();
+            case 5 -> exibirTodos();
+            case 6 -> salvarDados();
+            case 7 -> recuperarDados();
+            case 0 -> System.out.println("Finalizando...");
+            default -> System.out.println("\n ## Opção inválida! ##");
         }
     }
 
     private static void incluir() {
-    System.out.println("Incluir Pessoa (1 - Fisica, 2 - Juridica): ");
-    int tipo = scanner.nextInt();
-    scanner.nextLine();
-
-    if (tipo == 1) {
-        PessoaFisica pf = new PessoaFisica();
-        
-        System.out.println("Digite um ID: ");
-        pf.setId(parseInt(scanner.nextLine()));
-
-        System.out.println("Digite o nome: ");
-        pf.setNome(scanner.nextLine());
-
-        System.out.println("Digite o CPF: ");
-        pf.setCpf(scanner.nextLine());
-
-        System.out.println("Digite a idade: ");
-        int idade = scanner.nextInt();
+        System.out.println("Incluir Pessoa (1 - Fisica, 2 - Juridica): ");
+        int tipo = scanner.nextInt();
         scanner.nextLine();
 
-        pf.setIdade(idade);
+        if (tipo == 1) {
+            PessoaFisica pf = new PessoaFisica();
 
-        repoFisica.inserir(pf);
-        System.out.println("Pessoa Fisica adicionada com sucesso!");
-    } else if (tipo == 2) {
-        PessoaJuridica pj = new PessoaJuridica();
-        
-        System.out.println("Digite um ID: ");
-        pj.setId(parseInt(scanner.nextLine()));
+            System.out.println("Digite um ID: ");
+            pf.setId(parseInt(scanner.nextLine()));
 
-        System.out.println("Digite o nome:");
-        pj.setNome(scanner.nextLine());
+            System.out.println("Digite o nome: ");
+            pf.setNome(scanner.nextLine());
 
-        System.out.println("Digite o CNPJ:");
-        pj.setCnpj(scanner.nextLine());
+            System.out.println("Digite o CPF: ");
+            pf.setCpf(scanner.nextLine());
 
-        repoJuridica.inserir(pj);
-        System.out.println("Pessoa Juridica adicionada com sucesso!");
-    } else {
-        System.out.println("Tipo inválido.");
-    }
-}
-
-    private static void alterar() {
-    System.out.println("Alterar Pessoa (1 - Fisica, 2 - Juridica): ");
-    int tipo = scanner.nextInt();
-    scanner.nextLine();
-
-    System.out.println("Digite o ID da pessoa:");
-    int id = scanner.nextInt();
-    scanner.nextLine();
-
-    if (tipo == 1) {
-        PessoaFisica pf = repoFisica.obter(id);
-        if (pf != null) {
-            System.out.println("Dados atuais: ");
-            pf.exibir();
-
-            System.out.println("Digite o novo nome (deixe em branco para não alterar):");
-            String nome = scanner.nextLine();
-            if (!nome.isEmpty()) {
-                pf.setNome(nome);
-            }
-
-            System.out.println("Digite o novo CPF (deixe em branco para não alterar):");
-            String cpf = scanner.nextLine();
-            if (!cpf.isEmpty()) {
-                pf.setCpf(cpf);
-            }
-
-            System.out.println("Digite a nova idade (insira 0 para não alterar):");
+            System.out.println("Digite a idade: ");
             int idade = scanner.nextInt();
             scanner.nextLine();
-            if (idade != 0) {
-                pf.setIdade(idade);
-            }
 
-            repoFisica.alterar(pf);
-            System.out.println("Pessoa Fisica atualizada com sucesso!");
+            pf.setIdade(idade);
+
+            repoFisica.inserir(pf);
+            System.out.println("\n## Pessoa Fisica adicionada com sucesso! ##");
+        } else if (tipo == 2) {
+            PessoaJuridica pj = new PessoaJuridica();
+
+            System.out.println("Digite um ID: ");
+            pj.setId(parseInt(scanner.nextLine()));
+
+            System.out.println("Digite o nome:");
+            pj.setNome(scanner.nextLine());
+
+            System.out.println("Digite o CNPJ:");
+            pj.setCnpj(scanner.nextLine());
+
+            repoJuridica.inserir(pj);
+            System.out.println("\n## Pessoa Juridica adicionada com sucesso! ##");
         } else {
-            System.out.println("Pessoa Fisica não encontrada.");
+            System.out.println("\n## Tipo inválido. ##");
         }
-    } else if (tipo == 2) {
-        PessoaJuridica pj = repoJuridica.obter(id);
-        if (pj != null) {
-            System.out.println("Dados atuais: ");
-            pj.exibir();
-
-            System.out.println("Digite o novo nome (deixe em branco para nao alterar):");
-            String nome = scanner.nextLine();
-            if (!nome.isEmpty()) {
-                pj.setNome(nome);
-            }
-
-            System.out.println("Digite o novo CNPJ (deixe em branco para nao alterar):");
-            String cnpj = scanner.nextLine();
-            if (!cnpj.isEmpty()) {
-                pj.setCnpj(cnpj);
-            }
-
-            repoJuridica.alterar(pj);
-            System.out.println("Pessoa Juridica atualizada com sucesso!");
-        } else {
-            System.out.println("Pessoa Juridica não encontrada.");
-        }
-    } else {
-        System.out.println("Tipo invalido.");
     }
-}
 
+    private static void alterar() {
+        System.out.println("Alterar Pessoa (1 - Fisica, 2 - Juridica): ");
+        int tipo = scanner.nextInt();
+        scanner.nextLine();
+
+        System.out.println("Digite o ID da pessoa:");
+        int id = scanner.nextInt();
+        scanner.nextLine();
+
+        if (tipo == 1) {
+            PessoaFisica pf = repoFisica.obter(id);
+            if (pf != null) {
+                System.out.println("Dados atuais: ");
+                pf.exibir();
+
+                System.out.println("Digite o novo nome (deixe em branco para nao alterar):");
+                String nome = scanner.nextLine();
+                if (!nome.isEmpty()) {
+                    pf.setNome(nome);
+                }
+
+                System.out.println("Digite o novo CPF (deixe em branco para nao alterar):");
+                String cpf = scanner.nextLine();
+                if (!cpf.isEmpty()) {
+                    pf.setCpf(cpf);
+                }
+
+                System.out.println("Digite a nova idade (insira 0 para nao alterar):");
+                int idade = scanner.nextInt();
+                scanner.nextLine();
+                if (idade != 0) {
+                    pf.setIdade(idade);
+                }
+
+                repoFisica.alterar(pf);
+                System.out.println("\n## Pessoa Fisica atualizada com sucesso!");
+            } else {
+                System.out.println("\n## Pessoa Fisica não encontrada.");
+            }
+        } else if (tipo == 2) {
+            PessoaJuridica pj = repoJuridica.obter(id);
+            if (pj != null) {
+                System.out.println("Dados atuais: ");
+                pj.exibir();
+
+                System.out.println("Digite o novo nome (deixe em branco para nao alterar):");
+                String nome = scanner.nextLine();
+                if (!nome.isEmpty()) {
+                    pj.setNome(nome);
+                }
+
+                System.out.println("Digite o novo CNPJ (deixe em branco para nao alterar):");
+                String cnpj = scanner.nextLine();
+                if (!cnpj.isEmpty()) {
+                    pj.setCnpj(cnpj);
+                }
+
+                repoJuridica.alterar(pj);
+                System.out.println("\n## Pessoa Juridica atualizada com sucesso! ##");
+            } else {
+                System.out.println("\n## Pessoa Juridica não encontrada. ##");
+            }
+        } else {
+            System.out.println("\n## Tipo invalido. ##");
+        }
+    }
 
     private static void excluir() {
-        // Implementação para excluir uma entidade com base no tipo e ID
+        System.out.println("Excluir Pessoa (1 - Fisica, 2 - Juridica): ");
+        int tipo = scanner.nextInt();
+        scanner.nextLine(); // Limpar buffer do scanner
+
+        System.out.println("Digite o ID da pessoa a ser excluída:");
+        int id = scanner.nextInt();
+        scanner.nextLine(); // Limpar buffer do scanner
+
+        if (tipo == 1) {
+            PessoaFisica pf = repoFisica.obter(id);
+            if (pf != null) {
+                repoFisica.excluir(id);
+                System.out.println("\n## Pessoa Fisica removida com sucesso! ##");
+            } else {
+                System.out.println("\n## Pessoa Fisica não encontrada. ##");
+            }
+        } else if (tipo == 2) {
+            PessoaJuridica pj = repoJuridica.obter(id);
+            if (pj != null) {
+                repoJuridica.excluir(id);
+                System.out.println("\n## Pessoa Jurídica removida com sucesso! ##");
+            } else {
+                System.out.println("\n## Pessoa Jurídica não encontrada. ##");
+            }
+        } else {
+            System.out.println("\n## Tipo inválido. ##");
+        }
     }
 
     private static void exibirPorId() {
